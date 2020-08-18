2d930465b6e75e565cdb81a75e65ecb417b82dc7
neemiasbsilva <neemiasbsilva@gmail.com>
2020-08-18 17:51:57 -0400

    coding interview google (union-find)

--- coding-interview-google.py
+++ coding-interview-google.py
@@ -18,20 +18,22 @@ if __name__ == '__main__':
         id += 1
 
     pset = [i for i in range(len(synonyms))]
-    print(pset)
+    
     uf = union_find.UnionFind(pset)
 
     for i in range(0, len(pset), 2):
         uf.unionSet(list(synonyms.values())[i], list(synonyms.values())[i+1])
-    print(uf.pset)
+
+
     for i in range(int(input("Enter the number of querys: "))):
         strings = input().split(',')
         st_string = strings[0].split(' ')
         nd_string = strings[1].split(' ')
         equal = False
         for st_word, nd_word in zip(st_string, nd_string):
-            if (synonyms.get(st_word) != None and synonyms.get(nd_word) != None) and uf.issameSet(synonyms[st_word], synonyms[nd_word]):
-                equal = True
+            if (synonyms.get(st_word) != None and synonyms.get(nd_word) != None):
+                if uf.issameSet(synonyms[st_word], synonyms[nd_word]):
+                    equal = True
             elif st_word == nd_word :
                 equal = True
             else:
