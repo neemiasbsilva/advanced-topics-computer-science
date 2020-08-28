070845f195d651cf2b3e5bf7044bb0e6dfb2cfe1
neemiasbsilva <neemiasbsilva@gmail.com>
2020-08-28 14:46:24 -0400

    disjoint problem solved

---
+++ disjoint-problems/dark_road_1152.py
@@ -0,0 +1,44 @@
+import heapq
+
+class UnionFind:
+
+    def __init__(self, pset_length):
+        self.pset = [i for i in range(pset_length)]
+
+    def find_set(self, i):
+        if self.pset[i] == i: return i
+
+        self.pset[i] = self.find_set(self.pset[i])
+        return self.pset[i]
+
+    def union_set(self, i, j):
+        self.pset[self.find_set(i)] = self.find_set(j)
+
+    def issame_set(self, i, j):
+        if self.find_set(i) == self.find_set(j):
+            return True
+        else:
+            return False
+
+class Graph:
+
+    def __init__(self):
+        self.edge_list = []
+
+    def add_edge(self, u, v, w):
+        heapq.heappush(self.edge_list, w, (u, v))
+
+
+if __name__ == "__main__":
+
+    g = Graph()
+    m, n = int(input().split(' '))
+    while(n != 0):
+
+        x, y, z = int(input().split(' '))
+        
+        g.add_edge(x, y, z)
+
+        print(g.edge_list)
+
+    
---
+++ disjoint-problems/fusao_bancos.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+'''
+Input example:
+3 5
+C 1 2
+F 1 2
+C 1 2
+F 1 3
+C 1 3
+
+Output example:
+N
+S
+S
+'''
+
+
+class UnionFind:
+
+    def __init__(self, pset_length):
+        self.pset = [i for i in range(pset_length)]
+
+
+    def findSet(self, i):
+        if self.pset[i] == i:
+            return i
+
+        self.pset[i] = self.findSet(self.pset[i])
+        return self.pset[i]
+
+    def unionSet(self, i, j):
+        self.pset[self.findSet(i)] = self.findSet(j)
+
+    def issameSet(self, i, j):
+        if self.findSet(i) == self.findSet(j):
+            return True
+
+        return False
+
+
+if __name__ == "__main__":
+
+    N, K = list(input().split(' '))
+    N, K = int(N), int(K)
+
+    UF = UnionFind(N)
+
+    for i in range(K):
+        # example: C 1 2
+        ope, *line = input().split()
+        v1, v2 = list(map(int, line))
+        if ope == 'C':
+            if UF.issameSet(v1-1, v2-1):
+                print('S')
+            else:
+                print('N')
+        else:
+            UF.unionSet(v1-1, v2-1)
