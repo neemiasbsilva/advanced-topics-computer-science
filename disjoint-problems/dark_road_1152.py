30ddc716eab5b938513a2af3aa41a51fa5e6e594
neemiasbsilva <neemiasbsilva@gmail.com>
2020-08-29 16:42:07 -0400

    disjoint problem solved

--- disjoint-problems/dark_road_1152.py
+++ disjoint-problems/dark_road_1152.py
@@ -17,30 +17,42 @@ class UnionFind:
     def issame_set(self, i, j):
         if self.find_set(i) == self.find_set(j):
             return True
-        else:
-            return False
+        
+        return False
 
 class Graph:
 
-    def __init__(self):
+    def __init__(self, V):
+        self.V = V
         self.edge_list = []
 
     def add_edge(self, u, v, w):
-        heapq.heappush(self.edge_list,(w, [u, v]))
+        heapq.heappush(self.edge_list,[w, (u, v)])
 
+    def krukals_algorithm(self):
+        
+        s_edge_list = sorted(self.edge_list, key= lambda ele: ele[0])
+        
+        print(s_edge_list)
+        uf  = UnionFind(self.V)
+        cost = 0
+        for edge in s_edge_list:
+            if not uf.issame_set(edge[1][0], edge[1][1]):
+                cost += edge[0]
+                uf.union_set(edge[1][0], edge[1][1])
 
+        return cost
 if __name__ == "__main__":
 
-    g = Graph()
     m, n = list(map(int, input().split(' ')))
-
+    g = Graph(m)
     while(n != 0):
 
         for i in range(n):
             x, y, z = list(map(int,input().split(' ')))
             g.add_edge(x, y, z)
-
+        print(g.krukals_algorithm())
         m, n = list(map(int,input().split(' ')))
-        print(g.edge_list)
+        g = Graph(m)
 
     
