d0ddfb3ea71ff1f3aa6f384cb48bf69385961319
neemiasbsilva <neemiasbsilva@gmail.com>
2020-11-11 20:52:21 -0400

    final work - ice block problem

--- work/1034.py
+++ work/1034.py
@@ -1,7 +1,6 @@
 from sys import stdout, stdin, maxsize
 
-memory = [int(maxsize//2) for i in range(1000000+1)]
-    
+memory = [-1 for i in range(m+1)]
 def ice_block(m, arr):
 
     if m == 0: return 0
@@ -14,26 +13,27 @@ def ice_block(m, arr):
 
     return 1 + result
 
-def ice_block_dp(m, arr):
 
+def ice_block_dp(m, arr):
+    
     if m == 0: return 0
     elif m < 0: return int(maxsize//2)
-
+	
+	#Se já computou retorna.
+    if memory[m] != -1: return memory[m]
     result = int(maxsize)
 
     for i in range(len(arr)):
-        if memory[m - arr[i]] == int(maxsize//2) :
-            memory[m-arr[i]] = ice_block_dp(m - arr[i], arr)
+        result = min(result, ice_block_dp(m-arr[i], arr))
 
-        result = min(result, memory[m-arr[i]])
-
-
+    memory[m] = 1 + result
     return 1 + result
 
 if __name__ == "__main__":
    t = int(stdin.readline())
    for i in range(t):
-       memory = [int(maxsize//2) for i in range(1000000+1)]
+    #    memory = [int(maxsize//2) for i in range(1000000+1)]
+       memory = [-1 for i in range(m+1)]
        n, m =  stdin.readline().strip().split()
        n = int(n)
        m = int(m)
