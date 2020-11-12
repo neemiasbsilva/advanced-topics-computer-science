1e094669c231019d54b6cba04ec31c30d147c832
neemiasbsilva <neemiasbsilva@gmail.com>
2020-11-12 19:51:02 -0400

    transfer analogimon problem: dynamic program

--- work/transfer_analogimon.cpp
+++ work/transfer_analogimon.cpp
@@ -14,14 +14,18 @@ using namespace std;
 typedef vector<int> vi;
 
 int n;
-vi d((int)pow(10, 4)), p((int)pow(10, 4));
+vi d((int)pow(10, 4)+1), p((int)pow(10, 4)+1);
 
+int memory[10000+1][10000+1];
 
-int transfer_analogimon(int i, int k){
+int transfer_analogimon(int i, int k)
+{
     if(k >= 0 and i == n) return 0;
     else if(k < 0) return INF;
 
-    return max(transfer_analogimon(i+1, k), d[i] + transfer_analogimon(i+1, k-p[i]));
+    if(memory[i][k] != -1) return memory[i][k];
+    memory[i][k] = max(transfer_analogimon(i + 1, k), d[i] + transfer_analogimon(i + 1, k - p[i]));
+    return memory[i][k];
 }
 
 int main()
@@ -29,6 +33,9 @@ int main()
     int k;
 
     while(cin >> n >> k){
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= k; j++) memory[i][j] = -1;
+        }
         for(int i = 0; i < n; i++) cin >> d[i];
         for(int i = 0; i < n; i++) cin >> p[i];
 
