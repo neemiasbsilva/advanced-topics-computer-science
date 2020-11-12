ced750371a4d0564575b3ff6420db3ff4bd14348
neemiasbsilva <neemiasbsilva@gmail.com>
2020-11-12 15:17:08 -0400

    help vania problem: dynamic program

--- work/help_vania.cpp
+++ work/help_vania.cpp
@@ -14,12 +14,14 @@ typedef vector<int> vi;
 
 int n;
 vi w(1000+1), v(1000+1);
+int memory[1000 + 1][100 + 1];
 
 int help_vania(int i, int c){
     if(c >= 0 and i == n) return 0;
     else if(c < 0) return INF;
-
-    return max(help_vania(i+1, c), v[i] + help_vania(i+1, c - w[i]));
+    if(memory[i][c] != -1) return memory[i][c];
+    memory[i][c] = max(help_vania(i + 1, c), v[i] + help_vania(i + 1, c - w[i]));
+    return memory[i][c];
 }
 
 
@@ -31,7 +33,10 @@ int main(){
         h++;
         cin >> n >> c;
         if(n==0) break;
-
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= c; j++)
+                memory[i][j] = -1;
+        }
         for(int i = 0; i < n; i++) cin >> w[i] >> v[i];
 
         cout << "Caso " << h << ": " << help_vania(0, c) << endl;
